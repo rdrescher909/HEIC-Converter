1a773ce24d919a96688e700357da48441ff6428b
Robert Drescher <robert@macombhometeam.com>
2022-01-28 23:49:23 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# About
+
+A simple web interface that allows you to upload an heic image and it will return you a converted png of that image.
+
+## Setup and Install
+
+If you'd like to run this on your own machine, you'll need to clone the repository install the dependencies.
+
+### macOS/Linux
+
+`python3 -m pip install -r requirements.txt`
+
+### Windows
+
+`py -m pip install -r requirements.txt`
+
+
+
+Note that [pillow-heif](https://pypi.org/project/pillow-heif/) may need to build from source on your machine.
+
+
+
+## Contributing
+
+If you'd like to contribute, feel free to submit a pull request.
+
+
+
+
---
+++ heicconverter.py
@@ -0,0 +1,62 @@
+import os
+import tempfile
+from gevent.pywsgi import WSGIServer
+import requests
+import shutil
+
+from flask import Flask, request, render_template, send_file, after_this_request
+from PIL import Image
+import pillow_heif
+
+pillow_heif.register_heif_opener() # Give PIL access to HEIF
+
+UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = set(['heic', 'heif'])
+
+app = Flask(__name__)
+
+def is_allowed_file(filename: str) -> bool:
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def convert_file(output_dir, input_file):
+    image = Image.open(input_file)
+    image.save(f"{output_dir}/converted.png")
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    work_dir = tempfile.TemporaryDirectory()
+    file_name = "converted"
+    input_file_path = os.path.join(work_dir.name, file_name)
+    output_file_path = os.path.join(work_dir.name, file_name + ".png")
+
+    if request.method == "POST":
+        if 'file' not in request.files:
+            return "No image provided"
+        file = request.files['file']
+        if file.filename == "":
+            return "No image provided"
+        if file and is_allowed_file(file.filename):
+            file.save(input_file_path)
+    
+    if request.method == "GET":
+        url = request.args.get("url", type=str)
+        if not url:
+            return render_template('index.html')
+        # Download file from given url
+        response = requests.get(url, stream=True)
+        with open(input_file_path, "wb") as fp:
+            shutil.copyfileobj(response.raw, file)
+        del response
+    
+    convert_file(work_dir.name, input_file_path)
+
+    @after_this_request
+    def cleanup(response):
+        work_dir.cleanup()
+        return response
+
+    return send_file(output_file_path, mimetype="image/png")
+
+if __name__ == "__main__":
+    http_server = WSGIServer(('', int(os.environ.get("PORT", 8080))), app)
+    http_server.serve_forever()
